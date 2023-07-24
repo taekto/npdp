ae45be18cca001fcda662f284249baf9032c6339
aldms8960 <45choi22@gmail.com>
2023-07-24 09:29:20 +0900

    [FEAT/back] init project

---
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/NangPaDaePaApplication.java
@@ -0,0 +1,13 @@
+package com.project.npdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NangPaDaePaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NangPaDaePaApplication.class, args);
+    }
+
+}
