97ba70637e28bff1350148b80578f2054be20ebf
조해린 <stella9808@naver.com>
2023-07-27 03:15:05 +0900

    [FEAT/back] : jenkins build error test1

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/member/controller/MemberController.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/member/controller/MemberController.java
@@ -26,5 +26,5 @@ public class MemberController {
         return ResponseEntity.ok().body(authentication.getName() + "님의 글 등록 완료");
     }
     
-    // jenkins error 해결15
+    // jenkins build error 해결1
 }
