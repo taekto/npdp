fb63e9ba0ec11592c236174c25d0c4c8476a0022
조해린 <stella9808@naver.com>
2023-07-26 16:40:51 +0900

    [FEAT/back] : jenkins test3

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/member/service/MemberService.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/member/service/MemberService.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-//@Transactional
+@Transactional
 @RequiredArgsConstructor    // final 필드로 생성자 주입
 //@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberService {
@@ -31,20 +31,20 @@ public class MemberService {
     private Long expiredMs = 1000 * 60 * 60l;
 
     // 회원 가입
-//    public Long join(Member member){
-//        // 중복 가입 방지 확인
-//        validateDuplicateMember(member);
-//        memberRepository.save(member);
-//        return member.getId();
-//    }
+    public Long join(Member member){
+        // 중복 가입 방지 확인
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
 
     // 중복 가입 방지
-//    private void validateDuplicateMember(Member member) {
-//        List<Member> findMembers = memberRepository.findByEmail(member.getEmail());
-//        if(!findMembers.isEmpty()){
-//            throw new IllegalStateException("이미 존재하는 회원입니다.");
-//        }
-//    }
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByEmail(member.getEmail());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
 
     public String login(String email, String password){
         // 인증과정 추가
