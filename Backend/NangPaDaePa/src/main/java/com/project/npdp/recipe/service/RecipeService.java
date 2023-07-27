49a16b2517b59f52fcee66eeec1b2aa62d34177b
조해린 <stella9808@naver.com>
2023-07-27 10:13:31 +0900

    [FEAT/back] : jenkins build error test9

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/recipe/service/RecipeService.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/recipe/service/RecipeService.java
@@ -15,19 +15,19 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RecipeService {
 
-    private final RecipeRepository recipeRepository;
-    // 전체 레시피 조회
-    @Transactional(readOnly = true)
-    public List<Recipe> findAllRecipe() {
-        List<Recipe> all = recipeRepository.findAll();
-        return all;
-    }
-
-    // 레시피id로 레시피 조회
-    @Transactional(readOnly = true)
-    public Recipe findRecipe(Long recipeId) {
-        Recipe byId = recipeRepository.findById(recipeId).orElseThrow(() -> new IllegalArgumentException("레시피Id 조회불가"));
-        return byId;
-    }
+//    private final RecipeRepository recipeRepository;
+//    // 전체 레시피 조회
+//    @Transactional(readOnly = true)
+//    public List<Recipe> findAllRecipe() {
+//        List<Recipe> all = recipeRepository.findAll();
+//        return all;
+//    }
+//
+//    // 레시피id로 레시피 조회
+//    @Transactional(readOnly = true)
+//    public Recipe findRecipe(Long recipeId) {
+//        Recipe byId = recipeRepository.findById(recipeId).orElseThrow(() -> new IllegalArgumentException("레시피Id 조회불가"));
+//        return byId;
+//    }
     
 }
