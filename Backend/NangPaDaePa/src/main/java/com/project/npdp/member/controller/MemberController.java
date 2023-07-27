645b97fd23d833219114fd4433e0497b68c688a5
조해린 <stella9808@naver.com>
2023-07-27 10:00:01 +0900

    [FEAT/back] : jenkins build error test7

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/member/controller/MemberController.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/member/controller/MemberController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberService memberService;
-
-    // 로그인
-    @PostMapping("/login")
-    public ResponseEntity<String> login(String email, String password){
-        return ResponseEntity.ok().body(memberService.login(email, password));
-    }
-
-    @PostMapping("/write")
-    public ResponseEntity<String> writeSample(Authentication authentication){
-        return ResponseEntity.ok().body(authentication.getName() + "님의 글 등록 완료");
-    }
+//    private final MemberService memberService;
+//
+//    // 로그인
+//    @PostMapping("/login")
+//    public ResponseEntity<String> login(String email, String password){
+//        return ResponseEntity.ok().body(memberService.login(email, password));
+//    }
+//
+//    @PostMapping("/write")
+//    public ResponseEntity<String> writeSample(Authentication authentication){
+//        return ResponseEntity.ok().body(authentication.getName() + "님의 글 등록 완료");
+//    }
     
     // jenkins build error test6
 }
