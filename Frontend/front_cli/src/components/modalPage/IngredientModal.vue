2dc51c326df80c63916c2cc0f47172edc172e981
김민식 <a184562@gmail.com>
2023-07-27 14:48:21 +0900

    [FEAT/front] : 재료 및 양념 관리 모달창 완성

---
+++ Frontend/front_cli/src/components/modalPage/IngredientModal.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <IngredientText />
+    <IngredientSound />
+    <button class="modalButton" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-whatever="@ingredient">재료입력</button>
+  </div>
+  
+</template>
+
+<script>
+import IngredientText from '../modalPage/inputIngredientText.vue'
+import IngredientSound from '../modalPage/inputIngredientSound.vue'
+
+export default {
+  name : "IngredientModal",
+  components: {
+    IngredientText,
+    IngredientSound,
+  },
+
+  
+  
+    
+}
+</script>
+
+<style scoped>
+.modalButton {
+  border: solid #FD7E14;
+  color: white;
+  background-color: #FD7E14;
+  border-radius: .5rem;
+  margin: .5rem;
+  padding: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.amountButton {
+  width: 2rem;
+  height: 60%;
+  border-radius: .5rem;
+  background-color: #f2f2f2;
+  border: solid rgb(244, 244, 244);
+}
+
+.ListShow {
+  border-bottom: solid #a7a7a7;
+  width: 95%;
+  margin: auto;
+}
+
+.ingredientList {
+  display: flex;
+  border-bottom: solid grey;
+  
+}
+
+.modal-body {
+  display: flex;
+  margin: auto;
+  width: 100%;
+}
+
+
+.amount {
+  display: flex;
+}
+
+.inputComponent {
+  justify-content: space-around;
+}
+
+.radioButton {
+    border: solid #FD7E14;
+    border-radius: .5rem;
+    padding: .5rem;
+    margin: .5rem;
+    width: 6rem;
+}
+
+[type="radio"] {
+    appearance: none;
+}
+
+#searchForm {
+    border-radius: .5rem;
+    margin-right: 1rem;
+    margin-left: 1rem;
+}
+
+#ingredientText {
+  width: 10rem;
+}
+
+#submitButton {
+    width: 5rem;
+    border-radius: .5rem;
+    background-color: #f2f2f2;
+    border: solid rgb(244, 244, 244);
+}
+
+.servingButton {
+  border: solid #FD7E14;
+  background-color: white;
+  border-radius: .5rem;
+  width: 4rem;
+  padding: 0.25rem;
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+}
+
+.soundButton {
+  border: solid #FD7E14;
+  color: white;
+  background-color: #FD7E14;
+  border-radius: .5rem;
+  margin: 1rem;
+  padding: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+
+/* .modal {
+  width: 75%;
+} */
+/* 
+.modal-content {
+  width: 75%;
+} */
+
+</style>
