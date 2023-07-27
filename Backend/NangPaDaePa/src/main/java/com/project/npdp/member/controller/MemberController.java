4cac8a15cb7b1fd31babe19117ce7a09b5d8ddb1
조해린 <stella9808@naver.com>
2023-07-27 10:39:24 +0900

    [FEAT/back] : jenkins error test11

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/member/controller/MemberController.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/member/controller/MemberController.java
@@ -26,5 +26,5 @@ public class MemberController {
 //        return ResponseEntity.ok().body(authentication.getName() + "님의 글 등록 완료");
 //    }
     
-    // jenkins build error test10
+    // jenkins build error test11
 }
