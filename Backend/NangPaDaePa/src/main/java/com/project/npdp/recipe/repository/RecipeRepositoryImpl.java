0be88a174c2515bfa8bc5f487b56dee0a4660569
zosunny <zoheyy108@naver.com>
2023-08-01 01:35:12 +0900

    [FEAT/back] : JPAQueryFactory Bean Config 생성

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/recipe/repository/RecipeRepositoryImpl.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/recipe/repository/RecipeRepositoryImpl.java
@@ -6,13 +6,13 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.EntityManager;
 
 
-
+@RequiredArgsConstructor
 public class RecipeRepositoryImpl implements RecipeRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
-    public RecipeRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
+//    public RecipeRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
 
 }
