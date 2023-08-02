1ce0df3069c715df50cac9822c3fdc06f9b7a7ff
조해린 <stella9808@naver.com>
2023-08-02 15:27:52 +0900

    [FEAT/back] : modify ResponseDto

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/food/dto/response/IngredientResponseDto.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/food/dto/response/IngredientResponseDto.java
@@ -7,6 +7,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
+@Data
 public class IngredientResponseDto {
     private Long id;
     private String name;
