5d31c78a75df602096fe0aa862bda01e0436e08c
김민식 <a184562@gmail.com>
2023-08-03 23:52:44 +0900

    [FIx/Front] : 레시피 상세조회 수정

--- Frontend/front_cli/src/components/searchPage/searchResult.vue
+++ Frontend/front_cli/src/components/searchPage/searchResult.vue
@@ -12,7 +12,7 @@
         :key="recipe_item.recipe_id"
         >
             <!-- 레시피 카드로 표현 -->
-            <div class="recommendCard" @click="goToDetailRecipe(recipe_item.recipeId)">
+            <div class="recommendCard" @click="goToDetailRecipe(recipe_item)">
                 <img :src="recipe_item.imgBig" alt="">
                 <!-- <p>Recipe Name</p> -->
                 <!-- <img :src="recipe_item.img_small" alt=""> -->
