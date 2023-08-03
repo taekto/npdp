cc737ae4278a10ff6447a2ef2c3397bdd25f495c
김민식 <a184562@gmail.com>
2023-08-03 10:49:35 +0900

    [TEST/Front] : test 05

--- Frontend/front_cli/src/views/SignupView.vue
+++ Frontend/front_cli/src/views/SignupView.vue
@@ -60,6 +60,7 @@
           </div>
           <p>성별 : {{credentials.gender}}</p>
           <p>1234567987564165</p>
+          <p>1234567987564165</p>
       <button class="signup_btn" style="width: 100%;" @click="signup">Get started</button>
 
       <!-- 소셜 로그인 -->
