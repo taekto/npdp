9aa4c2207c4d07335819ccab5c9afe2ffd0430d9
박기택 <taekputer@gmail.com>
2023-08-07 12:57:26 +0900

    [FEAT/back] : cors

--- Backend/NangPaDaePa/src/main/java/com/project/npdp/configuration/CorsConfig.java
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/configuration/CorsConfig.java
@@ -11,10 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://i9b202.p.ssafy.io", "http://localhost:8080") // 허용할 도메인 설정
+                .allowedOrigins("*") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
                 .allowedHeaders("*") // 허용할 HTTP 헤더 설정
-                .allowCredentials(true) // 인증 헤더 허용 (옵션)
                 .maxAge(3600); // 캐시 시간 설정 (옵션)
     }
 }
