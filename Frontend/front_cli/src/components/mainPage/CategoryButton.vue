fce8bd71b23806d1dcdcdd492ef9c430916db1d6
김민식 <a184562@gmail.com>
2023-08-07 09:04:39 +0900

    [FIX/Front] : Category button temp

--- Frontend/front_cli/src/components/mainPage/CategoryButton.vue
+++ Frontend/front_cli/src/components/mainPage/CategoryButton.vue
@@ -1,18 +1,28 @@
 <template>
   <!-- 검색 결과 필터링 컴포넌트 -->
   <div class="categoryList">
-    <label><input type="radio" name="category" value="전체">전체</label>
-    <label><input type="radio" name="category" value="밥">밥</label>
-    <label><input type="radio" name="category" value="국/찌개">국/찌개</label>
-    <label><input type="radio" name="category" value="반찬">반찬</label>
-    <label><input type="radio" name="category" value="일품">일품</label>
-    <label><input type="radio" name="category" value="후식">후식</label>
+    <label v-if="selectCategory === '전체'" class="categotyButton"><input type="radio" name="category" value="전체" v-model="selectCategory">전체</label>
+    <label v-else class="categotyButton2"><input type="radio" name="category" value="전체" v-model="selectCategory">전체</label>
+    <label v-if="selectCategory === '밥'" class="categotyButton"><input type="radio" name="category" value="밥" v-model="selectCategory">밥</label>
+    <label v-else class="categotyButton2"><input type="radio" name="category" value="밥" v-model="selectCategory">밥</label>
+    <label v-if="selectCategory === '국/찌개'" class="categotyButton"><input type="radio" name="category" value="국/찌개" v-model="selectCategory">국/찌개</label>
+    <label v-else class="categotyButton2"><input type="radio" name="category" value="국/찌개" v-model="selectCategory">국/찌개</label>
+    <label v-if="selectCategory === '반찬'" class="categotyButton"><input type="radio" name="category" value="반찬" v-model="selectCategory">반찬</label>
+    <label v-else class="categotyButton2"><input type="radio" name="category" value="반찬" v-model="selectCategory">반찬</label>
+    <label v-if="selectCategory === '일품'" class="categotyButton"><input type="radio" name="category" value="일품" v-model="selectCategory">일품</label>
+    <label v-else class="categotyButton2"><input type="radio" name="category" value="일품" v-model="selectCategory">일품</label>
+    <label v-if="selectCategory === '후식'" class="categotyButton"><input type="radio" name="category" value="후식" v-model="selectCategory">후식</label>
+    <label v-else class="categotyButton2"><input type="radio" name="category" value="후식" v-model="selectCategory">후식</label>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      selectCategory : "전체"
+    }
+  }
 }
 </script>
 
@@ -34,5 +44,24 @@ export default {
 .categoryList label {
     margin-left: 1rem;
     margin-right: 1rem;
+}
+
+.categotyButton{
+  border: solid #FD7E14;
+  cursor: pointer;
+  background-color: #FD7E14;
+  color: white;
+  width: 9rem;
+  height: 3rem;
+  border-radius: .5rem;
+}
+
+.categotyButton2{
+  border: solid #FD7E14;
+  cursor: pointer;
+  color: #FD7E14;
+  width: 9rem;
+  height: 3rem;
+  border-radius: .5rem;
 }
 </style>
