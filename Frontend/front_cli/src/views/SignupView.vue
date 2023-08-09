08dc033b9e46e6e53bab7e110264197e525f5561
조해린 <stella9808@naver.com>
2023-08-09 11:05:56 +0900

    [FEAT/front] : 회원가입페이지 css 수정 완료

--- Frontend/front_cli/src/views/SignupView.vue
+++ Frontend/front_cli/src/views/SignupView.vue
@@ -4,31 +4,29 @@
     <div class="signup_card">
       <!-- 회원정보입력 -->
       <form class="signup_form" id="signup_form" @submit.prevent="localSignup(credentials)">
-        <h1 class="form_title">Craete an account</h1>
-          <label for="nickname" class="input_label">NickName</label>
+        <h1 class="form_title">회원가입</h1>
+          <label for="nickname" class="input_label">닉네임</label>
           <input
             type="text"
             id="nickname"
-            placeholder="Enter your NickName"
+            placeholder="닉네임을 입력해주세요"
             class="input"
             name="name"
-            style="width: 100%; height: 33.6px"
             v-model="credentials.nickname" 
           />
           
           <label for="email" class="input_label" 
           :class="{ 'title-danger': emailHasError }">
-          Email
+          이메일
           </label>
 
           <input
             type="email"
             id="email"
-            placeholder="Enter your Email"
+            placeholder="이메일을 입력해주세요"
             class="input-item"
             :class="{ 'input-danger': emailHasError }"
-            name="email"
-            style="width: 100%; height: 33.6px" 
+            name="email" 
             v-model="credentials.email"
           />
           <!-- 에러 메시지 표시 -->
@@ -38,15 +36,14 @@
 
           
 
-          <label for="password" class="input_label">Password</label>
+          <label for="password" class="input_label">비밀번호</label>
           <input
             type="password"
             id="password"
-            placeholder="Password"
+            placeholder="비밀번호를 입력해주세요"
             class="input-item"
             :class="{ 'input-danger':passwordHasError }"
             name="password"
-            style="width: 100%; height: 33.6px" 
             v-model="credentials.password"
           />
           <!-- 에러 메시지 표시 -->
@@ -59,16 +56,15 @@
              "8~16자리 숫자+영문자+특수문자 조합으로 입력해주세요!"
           </p>
 
-
+          <p>생일 선택</p>
           <VDatePicker 
           v-model="credentials.birth"
           :max="new Date()"
           name="birth"
           @dayclick="whatDate(credentials.birth)" />
-          <p>생일 : {{credentials.birth}}</p>
           <div class="genderSelect">  
             <div class="editCategoryTitle">
-              <p>성별 변경</p>
+              <p>성별 선택</p>
             </div>
             <div class="storageRadio">
               <label v-if="credentials.gender === 'none'" class="activeRadioButton">
@@ -91,19 +87,19 @@
               </label>
             </div>
           </div>
-      <button class="signup_btn" style="width: 100%;" @click="signup">Get started</button>
+      <button class="signup_btn" style="width: 100%;" @click="signup">회원가입</button>
       
 
       </form>
       <!-- 소셜 로그인 -->
-      <div class="signup_sns">
+      <!-- <div class="signup_sns">
           <a href="https://i9b202.p.ssafy.io/api/oauth/google-login?redirect_uri=http://i9B202.p.ssafy.io:8080/api/oauth/google" class="btn_sns btn-google btn-block" @click="socialLoginGoogle" style="width: 100%">
           Google 계정으로 시작</a>
           <a href="https://i9b202.p.ssafy.io/api/oauth/naver-login?redirect_uri=https://i9b202.p.ssafy.io/social" class="btn_sns btn-naver btn-block" @click="socialLoginNaver" style="width: 100%"><i class="fab fab-naver-alt"></i> 
           네이버 계정으로 시작</a>
           <a href="https://i9b202.p.ssafy.io/api/oauth/kakao-login?redirect_uri=https://i9b202.p.ssafy.io/social" class="btn_sns btn-kakao btn-block" @click="socialLoginKakao" style="width: 100%">
           KaKao 계정으로 시작</a>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -256,8 +252,24 @@ export default {
 </script>
 
 <style scoped>
+
+.signup_form {
+  width: 40%;
+}
+.signup_form input {
+  font-family: 'LINESeedKR-Rg';
+}
+
+.signup_form p {
+  font-family: 'LINESeedKR-Bd';
+  font-size: 19px;
+  margin-top: 2rem;
+}
+
+.radioButton {
+  border: 1.6px solid #FD7E14;
+}
 .activeRadioButton {
-  border: solid #FD7E14;
   background-color: #FD7E14;
   color: white;
   border-radius: .5rem;
@@ -266,6 +278,9 @@ export default {
   width: 6rem;
 }
 
+.activeRadioButton input {
+  border: 1px solid #FD7E14;
+}
 
 .signup_card {
   display: flex;
@@ -277,11 +292,15 @@ export default {
 }
 
 .form_title {
+  font-family: 'KimjungchulGothic-Bold';
   text-align: center;
-  margin-bottom: 15px;
+  margin-top: 70px;
+  margin-bottom: 70px;
 }
 
 .input_label {
+  font-family: 'LINESeedKR-Bd';
+  font-size: 18px;
   display: flex;
   padding: 15px 0px 5px 0px;
 }
@@ -301,18 +320,26 @@ export default {
   
 }
 .gender {
+  border: 1px solid #FD7E14;
   padding: 15px 0px 5px 0px;
 }
 
 .signup_btn {
-  margin-top: 25px;
-  font-size: 14px;
+  height: 3rem;
+  font-family: 'LINESeedKR-Rg';
+  margin-top: 50px;
+  margin-bottom: 50px;
+  font-size: 20px;
   border: none; 
   background-color: #FD7E14; 
   color: #FFFFFF;
-  padding: 7.5px;
+  padding: .5rem;
   cursor: pointer; 
   border-radius: 4px; 
+}
+
+.storageRadio {
+  font-family: 'LINESeedKR-Rg';
 }
 
 /* 소셜 로그인 */
@@ -340,7 +367,7 @@ export default {
     margin: 10px 0px 10px 0px;
 }
 
-.btn-google {
+/* .btn-google {
     color: #000;
     background-color: #ffffff;
     border-color: #ccc;
@@ -382,7 +409,7 @@ export default {
     font-family: Sans-Serif !important;
     min-width: 280px;
     background-image: url(../assets/Kakao.png);
-}
+} */
 
 /* 이메일 패스워드 에러 */
 .input-error {  
@@ -399,5 +426,14 @@ export default {
 
 .input-danger {
   border-bottom: 1px solid red !important;
+}
+
+.input, .input-item {
+  /* style="width: 100%; height: 33.6px" */
+  width: 100%;
+  height: 3rem;
+  border-radius: 3px;
+  border: 1px solid black;
+  padding: .5rem
 }
 </style>
