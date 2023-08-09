5878d59aea3c319b45d07dc270d4ad67cefe3b6b
김민식 <a184562@gmail.com>
2023-08-09 15:15:30 +0900

    [FIX/Front] : 소셜로그인 24

--- Frontend/front_cli/src/views/LoginView.vue
+++ Frontend/front_cli/src/views/LoginView.vue
@@ -46,10 +46,16 @@
       <div class="signup_sns">
           <button class="btn_sns btn-google btn-block" @click="socialLoginGoogle" style="width: 100%">
           Google 계정으로 시작</button>
+          <a href="https://i9b202.p.ssafy.io/api/oauth/google-login" class="btn_sns btn-google btn-block" @click="socialLoginGoogle" style="width: 100%">
+          Google 계정으로 시작</a>
           <button class="btn_sns btn-naver btn-block" @click="socialLoginNaver" style="width: 100%"><i class="fab fab-naver-alt"></i> 
           네이버 계정으로 시작</button>
+          <a href="https://i9b202.p.ssafy.io/api/oauth/naver-login" class="btn_sns btn-naver btn-block" @click="socialLoginNaver" style="width: 100%"><i class="fab fab-naver-alt"></i> 
+          네이버 계정으로 시작</a>
           <button  class="btn_sns btn-kakao btn-block" @click="socialLoginKakao" style="width: 100%">
           KaKao 계정으로 시작</button>
+          <a href="https://i9b202.p.ssafy.io/api/oauth/kakao-login" class="btn_sns btn-kakao btn-block" @click="socialLoginKakao" style="width: 100%">
+          KaKao 계정으로 시작</a>
       </div>
   </div>
   
