faedfb24396ad68e6b542961ee04513bb17c67e5
aldms8960 <45choi22@gmail.com>
2023-08-09 23:31:27 +0900

    [FIX/back] url 수정

--- Data/NangPaDaePa/NangPaDaePa/settings.py
+++ Data/NangPaDaePa/NangPaDaePa/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'data/static/'
+STATIC_ROOT = 'static_files/files/static/' # 요기
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
