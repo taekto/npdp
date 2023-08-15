f030435cc23239e2005aac68b5c31b89d9459ec1
zosunny <zoheyy108@naver.com>
2023-08-15 21:02:20 +0900

    [FEAT/back] : 유사도 Scheduling 기능 구현

---
+++ Backend/NangPaDaePa/src/main/java/com/project/npdp/member/service/DataUpdateService.java
@@ -0,0 +1,50 @@
+package com.project.npdp.member.service;
+
+import com.project.npdp.domain.MemberRecommend;
+import com.project.npdp.member.repository.DataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class DataUpdateService {
+
+    private final DataRepository dataRepository;
+    private final RestTemplate restTemplate;
+
+    // Django에서 비동기로 유사도 Get
+    @Async
+    public CompletableFuture<List<MemberRecommend>> fetchDataFromDjangoApi(){
+
+        String api = "https://i9b202.p.ssafy.io/data/recommend/user";
+
+        // 업데이트시 기존 데이터 모두 삭제
+        dataRepository.deleteAll();
+
+        ResponseEntity<MemberRecommend[]> responseEntity = restTemplate.getForEntity(api, MemberRecommend[].class);
+        if(responseEntity.getStatusCode().is2xxSuccessful()){
+            return CompletableFuture.completedFuture(Arrays.asList(responseEntity.getBody()));
+        }else{
+            throw new RuntimeException("유사도 데이터 업데이트에 실패했습니다.");
+        }
+
+    }
+
+
+    // 유사도 저장
+    @Transactional
+    public void updateMemberRecommend(List<MemberRecommend> memberRecommends){
+        for(MemberRecommend result : memberRecommends){
+            dataRepository.save(result);
+        }
+    }
+
+}
