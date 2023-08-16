bc14899e9041b6dae50b71ae93b10e92f9a716bd
김민식 <a184562@gmail.com>
2023-08-16 15:41:47 +0900

    [FIX/Front] : 자잘한 버그 수정3

--- Frontend/front_cli/src/components/myPage/refrigeratorPage.vue
+++ Frontend/front_cli/src/components/myPage/refrigeratorPage.vue
@@ -40,7 +40,6 @@
             <div>
                 <!-- 재료 -->
                 <div class="refrigeratorCategory">
-                    <h1>{{memberIngredient}}</h1>
                     <div style="display: flex; justify-content: space-between; width: 80%; margin: auto">
                         <p class="categoryTitle">{{printStorage}} 재료</p>
                         <button class="saveButton" @click="saveMaterial({type: 'ingredient', memberId: this.memberId, sendData: displayedIngredientItems.tempIngredient })">
